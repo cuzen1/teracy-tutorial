e8b44c5970ad73139a8edf016b512a78c5f5f418
cuzen1 <cuzen2201@gmail.com>
2015-04-23 15:16:52 +0000

    Raising a 404 error

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from apps.polls.models import Poll
 from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -9,7 +10,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/details.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)

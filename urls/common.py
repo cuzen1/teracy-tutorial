63ee8bf45f3e9e709f881350a4af9c9544b4f717
cuzen1 <cuzen2201@gmail.com>
2015-05-07 04:44:07 +0000

    Namespacing URL names

--- urls/common.py
+++ urls/common.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, include, url  # noqa
 
 urlpatterns = patterns(
     '',
-	url(r'^polls/', include('apps.polls.urls')),
+	url(r'^polls/', include('apps.polls.urls', namespace="polls")),
     # Examples:
     # url(r'^$', 'teracy.views.home', name='home'),
     # url(r'^teracy/', include('teracy.foo.urls')),
